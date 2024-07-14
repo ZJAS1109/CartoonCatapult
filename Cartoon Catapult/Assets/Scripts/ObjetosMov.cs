d2d12011fa1a6e0704196828d4fcdb98f4240fc9
ZadhArg <zadharg@gmail.com>
2024-07-14 04:09:13 -0600

    Movimiento Objetos
    Se programo el movimiento de los objetos.

---
+++ Cartoon Catapult/Assets/Scripts/ObjetosMov.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjetosMov : MonoBehaviour
+{
+    public List<Transform> destinos;
+
+    public float velocidad = 5f;
+
+    private int i = 0;
+
+    
+
+    void Update(){
+
+        Transform destinoActual = destinos[i];
+
+        transform.position = Vector3.MoveTowards(transform.position, destinoActual.position, velocidad * Time.deltaTime);
+
+        if (Vector3.Distance(transform.position, destinoActual.position) < 0.1f){
+
+            i = (i +1) % destinos.Count;
+        }
+    }
+
+
+}
